87f6ee62bd2ef0e7f605344280cf8af2b66cbd59
Stephen Tollenaar <stephen@tollenaar.com>
2023-09-20 16:17:26 -0230

    adding sqspoller

--- cmd/sqspoller/go.mod
+++ cmd/sqspoller/go.mod
@@ -1,3 +1,46 @@
 module github.com/stollenaar/cmd/sqspoller
 
 go 1.20
+
+require (
+	github.com/aws/aws-sdk-go-v2 v1.21.0
+	github.com/aws/aws-sdk-go-v2/config v1.18.40
+	github.com/aws/aws-sdk-go-v2/service/sqs v1.24.5
+	github.com/stollenaar/copypastabotv2/pkg/markov v0.0.0-20230920184355-67a0004bd0bd
+	github.com/stollenaar/statisticsbot/util v0.0.0-20230920181313-1f20a73ca6d1
+)
+
+require (
+	github.com/PuerkitoBio/goquery v1.8.0 // indirect
+	github.com/andybalholm/cascadia v1.3.1 // indirect
+	github.com/aws/aws-sdk-go-v2/credentials v1.13.38 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.13.11 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.1.41 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.4.35 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.3.42 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.9.35 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ssm v1.37.5 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.14.0 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.16.0 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.22.0 // indirect
+	github.com/aws/smithy-go v1.14.2 // indirect
+	github.com/bwmarrin/discordgo v0.27.1 // indirect
+	github.com/golang/protobuf v1.2.0 // indirect
+	github.com/golang/snappy v0.0.1 // indirect
+	github.com/google/go-querystring v1.0.0 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/klauspost/compress v1.13.6 // indirect
+	github.com/stollenaar/copypastabotv2/internal/util v0.0.0-20230919181553-5058c04f7559 // indirect
+	github.com/vartanbeno/go-reddit/v2 v2.0.1 // indirect
+	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
+	go.mongodb.org/mongo-driver v1.12.1 // indirect
+	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d // indirect
+	golang.org/x/net v0.0.0-20220722155237-a158d28d115b // indirect
+	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d // indirect
+	golang.org/x/sync v0.0.0-20220722155255-886fb9371eb4 // indirect
+	golang.org/x/sys v0.0.0-20220722155257-8c9f86f7a55f // indirect
+	golang.org/x/text v0.7.0 // indirect
+	google.golang.org/appengine v1.4.0 // indirect
+)
--- cmd/sqspoller/sqspoller.go
+++ cmd/sqspoller/sqspoller.go
@@ -1,7 +1,144 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/sqs"
+	"github.com/aws/aws-sdk-go-v2/service/sqs/types"
+	"github.com/stollenaar/copypastabotv2/pkg/markov"
+	"github.com/stollenaar/statisticsbot/util"
+)
+
+var (
+	sqsClient        *sqs.Client
+	sqsObjectChannel chan util.SQSObject
+
+	sqsRequestURL  *string
+	sqsResponseURL *string
+)
+
+func init() {
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		panic("configuration error, " + err.Error())
+	}
+
+	sqsClient = sqs.NewFromConfig(cfg)
+	sqsObjectChannel = make(chan util.SQSObject)
+
+	// Get URL of request queue
+	urlResult, err := sqsClient.GetQueueUrl(context.TODO(), &sqs.GetQueueUrlInput{
+		QueueName: &util.ConfigFile.SQS_REQUEST,
+	})
+	if err != nil {
+		fmt.Println("Got an error getting the queue URL:")
+		panic(err)
+	}
+
+	sqsRequestURL = urlResult.QueueUrl
+
+	// Get URL of response queue
+	urlResult, err = sqsClient.GetQueueUrl(context.TODO(), &sqs.GetQueueUrlInput{
+		QueueName: &util.ConfigFile.SQS_RESPONSE,
+	})
+	if err != nil {
+		fmt.Println("Got an error getting the queue URL:")
+		panic(err)
+	}
+
+	sqsResponseURL = urlResult.QueueUrl
+
+	go pollSQS(sqsObjectChannel)
+}
 
 func main() {
-	fmt.Println("hello")
+	for sqsObject := range sqsObjectChannel {
+		switch sqsObject.Type {
+		case "url":
+			handleURLObject(sqsObject)
+		case "user":
+			handleUserObject(sqsObject)
+		default:
+			fmt.Printf("Unknown type has been send to queue. sqsObject is: %v", sqsObject)
+		}
+	}
+}
+
+func pollSQS(chl chan<- util.SQSObject) {
+	for {
+		msgResult, err := sqsClient.ReceiveMessage(context.TODO(), &sqs.ReceiveMessageInput{
+			MessageAttributeNames: []string{
+				string(types.QueueAttributeNameAll),
+			},
+			QueueUrl:            sqsRequestURL,
+			MaxNumberOfMessages: 1,
+			VisibilityTimeout:   int32(5),
+		})
+		if err != nil {
+			fmt.Println("Got an error receiving messages:")
+			fmt.Println(err)
+		}
+
+		for _, message := range msgResult.Messages {
+			var object util.SQSObject
+			json.Unmarshal([]byte(*message.Body), &object)
+			chl <- object
+		}
+	}
+}
+
+func handleURLObject(sqsObject util.SQSObject) {
+	response := util.SQSObject{
+		Type:          sqsObject.Type,
+		Command:       sqsObject.Command,
+		GuildID:       sqsObject.GuildID,
+		Token:         sqsObject.Token,
+		ApplicationID: sqsObject.ApplicationID,
+	}
+	content, err := markov.GetMarkovURL(sqsObject.Data)
+	if err != nil {
+		response.Data = fmt.Errorf("something went wrong: %w", err).Error()
+	} else {
+		response.Data = content
+	}
+	data, err := json.Marshal(response)
+	if err != nil {
+		fmt.Printf("Error marshalling response object: %v", err)
+		return
+	}
+	sqsClient.SendMessage(context.TODO(), &sqs.SendMessageInput{
+		MessageBody: aws.String(string(data)),
+		QueueUrl:    sqsResponseURL,
+	})
+}
+
+func handleUserObject(sqsObject util.SQSObject) {
+	response := util.SQSObject{
+		Type:          sqsObject.Type,
+		Command:       sqsObject.Command,
+		GuildID:       sqsObject.GuildID,
+		Token:         sqsObject.Token,
+		ApplicationID: sqsObject.ApplicationID,
+	}
+
+	content, err := markov.GetUserMarkov(sqsObject.GuildID, sqsObject.Data)
+	if err != nil {
+		response.Data = fmt.Errorf("something went wrong: %w", err).Error()
+	} else {
+		response.Data = content
+	}
+
+	data, err := json.Marshal(response)
+	if err != nil {
+		fmt.Printf("Error marshalling response object: %v", err)
+		return
+	}
+	sqsClient.SendMessage(context.TODO(), &sqs.SendMessageInput{
+		MessageBody: aws.String(string(data)),
+		QueueUrl:    sqsResponseURL,
+	})
 }
