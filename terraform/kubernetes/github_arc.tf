dae63c37b3fd5671a3518403538cdae18c0ede41
Stephen Tollenaar <stephen@tollenaar.com>
2025-05-11 16:44:53 -0230

    changing containerMode

--- terraform/kubernetes/github_arc.tf
+++ terraform/kubernetes/github_arc.tf
@@ -3,19 +3,9 @@ resource "helm_release" "github_arc_runner_statisticsbot" {
   namespace  = data.terraform_remote_state.kubernetes_cluster.outputs.github_arc.namespace.metadata.0.name
   repository = "oci://ghcr.io/actions/actions-runner-controller-charts"
   chart      = "gha-runner-scale-set"
-  version    =  data.terraform_remote_state.kubernetes_cluster.outputs.github_arc.version
-  values     = []
-
-  set {
-    name  = "githubConfigUrl"
-    value = "https://github.com/STollenaar/statisticsBot"
-  }
-  set {
-    name  = "githubConfigSecret"
-    value =  data.terraform_remote_state.kubernetes_cluster.outputs.github_arc.secret_name
-  }
-  set {
-    name = "containerMode.type"
-    value = "dind"
-  }
+  version    = data.terraform_remote_state.kubernetes_cluster.outputs.github_arc.version
+  values = [templatefile("${path.module}/conf/arc-runner-values.yaml", {
+    github_config_url = "https://github.com/STollenaar/statisticsBot"
+    github_secret     = data.terraform_remote_state.kubernetes_cluster.outputs.github_arc.secret_name
+  })]
 }
