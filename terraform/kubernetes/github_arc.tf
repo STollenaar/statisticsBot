32d2153ccf509d37791acf97aaeda2db39aa9f57
Stephen Tollenaar <stephen@tollenaar.com>
2025-05-15 18:46:38 -0230

    tweaking config

--- terraform/kubernetes/github_arc.tf
+++ terraform/kubernetes/github_arc.tf
@@ -9,3 +9,22 @@ resource "helm_release" "github_arc_runner_statisticsbot" {
     github_secret     = data.terraform_remote_state.kubernetes_cluster.outputs.github_arc.secret_name
   })]
 }
+
+resource "kubernetes_role_binding" "github_arc_admin" {
+  metadata {
+    name      = "namespace-admin-binding"
+    namespace = kubernetes_namespace.statisticsbot.metadata.0.name
+  }
+
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = "admin"
+  }
+
+  subject {
+    kind      = "ServiceAccount"
+    name      = "arc-runner-statisticsbot-gha-rs-kube-mode"
+    namespace = data.terraform_remote_state.kubernetes_cluster.outputs.github_arc.namespace.metadata.0.name
+  }
+}
