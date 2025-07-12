335ce8606b6c6f62980adceb69a90e947778d2c6
Stephen Tollenaar <stephen@tollenaar.com>
2025-07-12 18:35:51 -0230

    fix some of Gin warnings

--- internal/routes/router.go
+++ internal/routes/router.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/bwmarrin/discordgo"
 	"github.com/gin-gonic/gin"
+	"github.com/stollenaar/statisticsbot/internal/util"
 )
 
 var (
@@ -13,7 +14,11 @@ var (
 
 func CreateRouter(b *discordgo.Session) {
 	bot = b
+	if !util.ConfigFile.DEBUG {
+		gin.SetMode(gin.ReleaseMode)
+	}
 	r := gin.Default()
+	r.SetTrustedProxies(nil)
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
